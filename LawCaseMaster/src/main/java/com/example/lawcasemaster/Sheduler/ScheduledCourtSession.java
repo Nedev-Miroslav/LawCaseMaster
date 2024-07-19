91348d0295039586f5ecc0f94548d28c8a1f9864
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-07-19 18:33:52 +0300

    small fix Scheduled

--- LawCaseMaster/src/main/java/com/example/lawcasemaster/Sheduler/ScheduledCourtSession.java
+++ LawCaseMaster/src/main/java/com/example/lawcasemaster/Sheduler/ScheduledCourtSession.java
@@ -1,4 +1,4 @@
-package com.example.lawcasemaster.Sheduler;
+package com.example.lawcasemaster.sheduler;
 
 import com.example.lawcasemaster.service.CourtSessionService;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -16,7 +16,8 @@ public class ScheduledCourtSession {
         this.courtSessionService = courtSessionService;
     }
 
-    @Scheduled(cron = "0 0 0 * * ?")// Всеки ден в полунощ
+//    @Scheduled(cron = "0 * * * * ?")    // На всяка минута за да си тествам
+    @Scheduled(cron = "0 0 0 * * ?")   // Всеки ден в полунощ
     public void deleteOldCourtSessions() {
         courtSessionService.deletePastSessions();
         System.out.println("Deleted past court sessions at " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
